de44c3cd93cd4492e5e55021cc8f60f7da26a0fb
Amit Hasan Sadhin <71214071+samithasan7866@users.noreply.github.com>
2021-10-18 12:54:18 +0600

    Add files via upload

---
+++ Operators/Oprtators.js
@@ -0,0 +1,67 @@
+/*let num1=10;
+let num2=2;
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1 ** num2);//Power
+
+console.log(num1 % num2);
+console.log(36 % 4);
+
+*/
+//Assignment Operator 
+/* let num1=10;
+let num2= num1;
+
+console.log(num1, num2);
+
+num1 = num1 + 5;
+num1 += 5;
+
+num1 = num1 * 5;
+num1 *= 5;
+
+num1 = num1 + 1;
+num1 += 1;
+num1++; */
+
+//Comparison Operators
+/*
+let price = 100;
+
+console.log(price>200);
+console.log(price >= 100);
+console.log(price<50);
+console.log(price<= 500);
+console.log(price === 100);
+console.log(price !== 100);
+*/
+//logical Operators
+/*
+let price = 10;
+console.log(price > 5 && price<15);
+console.log(price > 50 && price<15);
+console.log(price > 5 || price<15);
+console.log(!(price>50));
+console.log();
+console.log();
+*/
+
+//Equality Operators
+/*
+let price = 10;
+console.log(price === 10);
+console.log(price !== 20);
+*/
+
+//Swap two numbers
+
+
+let a=10;
+let b=20;
+let temp =a;//temp =10
+a=b;
+b=temp;
+console.log(a,b);
